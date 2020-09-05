1610982410ecf30e93dfd6c060495209eef80ef6
galegosimpatico <galegosimpatico@outlook.com>
2020-09-05 10:26:04 +0200

    wip

--- db/migrate/20200904224806_add_body_markdown_before_to_suggested_edit.rb
+++ db/migrate/20200904224806_add_body_markdown_before_to_suggested_edit.rb
@@ -1,4 +1,4 @@
-# rails generate migration AddBodyMarkdownBeforeToSuggestedEdit body_markdown_before:text
+# $ rails generate migration AddBodyMarkdownBeforeToSuggestedEdit body_markdown_before:text
 class AddBodyMarkdownBeforeToSuggestedEdit < ActiveRecord::Migration[5.2]
   def change
     add_column :suggested_edits, :body_markdown_before, :text
