235e39bb9fef051969e43968979e2079a4874d7c
galegosimpatico <galegosimpatico@outlook.com>
2020-09-05 10:26:12 +0200

    wip

--- db/migrate/20200905082144_add_title_before_to_suggested_edit.rb
+++ db/migrate/20200905082144_add_title_before_to_suggested_edit.rb
@@ -1,3 +1,4 @@
+# $ rails generate migration AddTitleBeforeToSuggestedEdit title_before:text
 class AddTitleBeforeToSuggestedEdit < ActiveRecord::Migration[5.2]
   def change
     add_column :suggested_edits, :title_before, :text
